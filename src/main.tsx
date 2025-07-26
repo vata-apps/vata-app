fa322ed084af6d34fb09f19208e1420c5e6a3cbf
Steve Bourgoin <stiv.aw@gmail.com>
2025-07-26 16:09:54 -0400

    feat: integrate TanStack Router Devtools for enhanced debugging capabilities

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { queryClient } from "./lib/query-client";
@@ -16,6 +17,7 @@ async function initializeApp() {
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <TanStackRouterDevtools router={router} />
       </QueryClientProvider>
     </React.StrictMode>,
   );
