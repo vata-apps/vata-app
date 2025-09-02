139053829bd16cade8c45db72a5fa0d93f3c04e3
Steve Bourgoin <stiv.aw@gmail.com>
2025-09-02 09:25:00 -0400

    refactor: remove Mantine CSS imports from RootLayout (managed elsewhere)

--- src/router/layouts/RootLayout.tsx
+++ src/router/layouts/RootLayout.tsx
@@ -2,9 +2,6 @@ import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import { Outlet } from "@tanstack/react-router";
 
-import "@mantine/core/styles.css";
-import "@mantine/notifications/styles.css";
-
 export function RootLayout() {
   return (
     <MantineProvider defaultColorScheme="dark">
