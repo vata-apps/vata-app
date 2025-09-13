3c38d9e3e243ca6b6650da26440b587e13ef5ee8
Steve Bourgoin <stiv.aw@gmail.com>
2025-09-13 17:00:22 -0400

    refactor: temporarily remove frontend UI for backend focus

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MantineProvider, createTheme } from "@mantine/core";
-import { router } from "./router";
 import { ThemeProvider } from "./lib/theme/ThemeProvider";
 
 import "@mantine/core/styles.css";
@@ -25,7 +23,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
             defaultColorScheme="auto"
             forceColorScheme={mantineColorScheme}
           >
-            <RouterProvider router={router} />
+            APP
           </MantineProvider>
         )}
       </ThemeProvider>
