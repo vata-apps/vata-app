f449190f99c630b8f71393980ce9612e502f0288
Steve Bourgoin <stiv.aw@gmail.com>
2026-04-26 14:57:21 -0400

    feat(ui): mount Toaster at app root
    Closes part of #57.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { Toaster } from '$components/ui/sonner';
 import { queryClient } from './lib/query-client';
 import { routeTree } from './routeTree.gen';
 
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster />
     </QueryClientProvider>
   </React.StrictMode>
 );
