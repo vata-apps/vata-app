3047b50748032fbbfe96b874b1ccb6903704a102
Steve Bourgoin <stiv.aw@gmail.com>
2026-04-28 16:56:00 -0400

    chore(ui): remove styled components and theme sync hook

--- src/components/layouts/MainLayout.tsx
+++
@@ -1,7 +0,0 @@
-interface MainLayoutProps {
-  children: React.ReactNode;
-}
-
-export function MainLayout({ children }: MainLayoutProps) {
-  return <div style={{ minHeight: '100vh' }}>{children}</div>;
-}
