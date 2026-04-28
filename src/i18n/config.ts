6c7df72975f81f03d9352ffcddf39a104c33b952
Steve Bourgoin <stiv.aw@gmail.com>
2026-04-28 16:59:47 -0400

    chore(i18n): prune translation keys to scaffold minimum

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -3,43 +3,15 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 import enCommon from './locales/en/common.json';
-import enFamilies from './locales/en/families.json';
-import enHome from './locales/en/home.json';
-import enIndividuals from './locales/en/individuals.json';
-import enRepositories from './locales/en/repositories.json';
-import enSources from './locales/en/sources.json';
-import enWorkspace from './locales/en/workspace.json';
 import frCommon from './locales/fr/common.json';
-import frFamilies from './locales/fr/families.json';
-import frHome from './locales/fr/home.json';
-import frIndividuals from './locales/fr/individuals.json';
-import frRepositories from './locales/fr/repositories.json';
-import frSources from './locales/fr/sources.json';
-import frWorkspace from './locales/fr/workspace.json';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      en: {
-        common: enCommon,
-        families: enFamilies,
-        home: enHome,
-        individuals: enIndividuals,
-        repositories: enRepositories,
-        sources: enSources,
-        workspace: enWorkspace,
-      },
-      fr: {
-        common: frCommon,
-        families: frFamilies,
-        home: frHome,
-        individuals: frIndividuals,
-        repositories: frRepositories,
-        sources: frSources,
-        workspace: frWorkspace,
-      },
+      en: { common: enCommon },
+      fr: { common: frCommon },
     },
     defaultNS: 'common',
     fallbackLng: 'en',
